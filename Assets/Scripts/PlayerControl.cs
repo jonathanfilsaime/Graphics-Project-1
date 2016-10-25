3931591a0c04ff05c5a715c862421c42ae34a74c
phantomlaof <phantomloaf@yahoo.com>
2016-10-25 17:25:52 -0500

    Game ends when he falls through the cracks

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,78 +3,82 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
-    [HideInInspector]
-    public bool isUp = false;
-    [HideInInspector]
-    public bool isDown = false;
+	[HideInInspector]
+	public bool isUp = false;
+	[HideInInspector]
+	public bool isDown = false;
 
-    public float gravity;
-    public float speed;
-    public float maxSpeed;
+	public float gravity;
+	public float speed;
+	public float maxSpeed;
 
-    private Animator anim;
-    private Rigidbody rb;
-    private Collider coll;
-    private CharacterController controller;
+	private Animator anim;
+	private Rigidbody rb;
+	private Collider coll;
+	private CharacterController controller;
 
-    private Vector3 moveDirection = Vector3.zero;
-    private float verticalSpeed = 0;
+	public Vector3 moveDirection = Vector3.zero;
+	private float verticalSpeed = 0;
 
-    void Awake() {
-        //coll = GetComponent<Collider>();
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
-        controller = GetComponent<CharacterController>();
-    }
+	void Awake() {
+		//coll = GetComponent<Collider>();
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+		controller = GetComponent<CharacterController>();
+	}
 
-    // Use this for initialization
-    void Start()
-    {
-    }
+	// Use this for initialization
+	void Start()
+	{
+	}
 
-    // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Space) && (this.isUp || this.isDown)) // change to "Reverse"
-        {
-            this.isUp = false;
-            this.isDown = false;
-            Flip();
-        }
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space) && (this.isUp || this.isDown)) // change to "Reverse"
+		{
+			this.isUp = false;
+			this.isDown = false;
+			Flip();
+		}
 
-        moveDirection = Vector3.right;
-        verticalSpeed -= gravity * Time.deltaTime;
+		moveDirection = Vector3.right;
+		verticalSpeed -= gravity * Time.deltaTime;
 
-        // if speed > 0
-        // line cast up
-        // else if speed < 0
-        // line cast down
-        if (verticalSpeed > 0) {
-            if(Physics.Linecast(this.gameObject.transform.position, this.gameObject.transform.position + new Vector3(0, 0.6f, 0))) {
-                this.isUp = true;
-                verticalSpeed = 0;
-            } else {
-                this.isUp = false;
-            }
-        } else if(verticalSpeed < 0) {
-            if (Physics.Linecast(this.gameObject.transform.position, this.gameObject.transform.position - new Vector3(0, 0.6f, 0))) {
-                this.isDown = true;
-                verticalSpeed = 0;
-            } else {
-                this.isDown = false;
-            }
-        }
+		// if speed > 0
+		// line cast up
+		// else if speed < 0
+		// line cast down
+		if (verticalSpeed > 0) {
+			if(Physics.Linecast(this.gameObject.transform.position, this.gameObject.transform.position + new Vector3(0, 0.6f, 0))) {
+				this.isUp = true;
+				verticalSpeed = 0;
+			} else {
+				this.isUp = false;
+			}
+		} else if(verticalSpeed < 0) {
+			if (Physics.Linecast(this.gameObject.transform.position, this.gameObject.transform.position - new Vector3(0, 0.6f, 0))) {
+				this.isDown = true;
+				verticalSpeed = 0;
+			} else {
+				this.isDown = false;
+			}
+		}
 
-        moveDirection *= speed;
-        moveDirection.y = verticalSpeed;
-        controller.Move(moveDirection * Time.deltaTime);
-    }
+		moveDirection *= speed;
+		moveDirection.y = verticalSpeed;
+		controller.Move(moveDirection * Time.deltaTime);
 
-    void FixedUpdate() {
+		if (controller.height > 27 || controller.height < 0)
+			verticalSpeed = 0;
 
-    }
+	}
 
-    void Flip() {
-        this.gravity = -this.gravity;
+	void FixedUpdate() {
+
+	}
+
+	void Flip() {
+		this.gravity = -this.gravity;
 		GetComponent<SpriteRenderer> ().flipY = !GetComponent<SpriteRenderer> ().flipY;
-    }
+	}
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
     public Text scoreText;
     public Text livesText;
     public Text speedText;
+	public GameObject gameOverPanel;
+	public Text gameOverText;
 
     private int score;
     private float speed;
+	public int scene; //make better
+	public Text text; //make better
+
+	private AsyncOperation async;
  	// Use this for initialization
+
+	void awake()
+	{
+		gameOverPanel.SetActive (false);
+	}
+
 	void Start () {
         score = 0;
         speed = GameObject.Find("Player").GetComponent<PlayerControl>().speed;
@@ -22,5 +35,16 @@ public class UIController : MonoBehaviour {
 
         speed = GameObject.Find("Player").GetComponent<PlayerControl>().speed;
         speedText.text = "Speed: " + speed;
+
+		if (GameObject.Find ("Player").GetComponent<PlayerControl> ().moveDirection.y > 30 ||
+		    GameObject.Find ("Player").GetComponent<PlayerControl> ().moveDirection.y < -30) {
+			StartCoroutine(LoadNewScene());
+		}
+	}
+
+	IEnumerator LoadNewScene()
+	{
+		async = SceneManager.LoadSceneAsync(scene);
+		yield return null; //loads when done
 	}
 }
